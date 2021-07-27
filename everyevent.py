204c56c3c31fba820d55a1491095b8b8ea639c34
Harry P <habs@sdf.org>
2021-07-27 00:57:03 -0400

    add events

--- everyevent.py
+++ everyevent.py
@@ -10,6 +10,7 @@ events = [
   '1500 metres',
   '5000 metres',
   '10,000 metres',
+  '5 miles'
 ]
 
 for year in sorted(os.listdir('html')):
@@ -19,6 +20,6 @@ for year in sorted(os.listdir('html')):
     td = soup.select_one('td:-soup-contains("{}")'.format(evt))
     if td:
       evtmedals = { a.text for a in td.parent.find_all('a', href = re.compile('/wiki/.*_at_the_.*_Summer_Olympics$')) }
-      if medalists is None: medalists = list(evtmedals)
-      else: medalists = [ cnt for cnt in medalists if cnt in evtmedals ]
+      if medalists is None: medalists = evtmedals
+      else: medalists = { cnt for cnt in medalists if cnt in evtmedals }
   if (len(medalists) > 0): print(year, medalists)
