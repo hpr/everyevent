aa3cb8cf0756d3fadf21a8380f658f09d8d8343f
Harry P <habs@sdf.org>
2021-07-27 01:45:28 -0400

    add women

--- everyevent.py
+++ everyevent.py
@@ -13,13 +13,18 @@ events = [
   '5 miles'
 ]
 
-for year in sorted(os.listdir('html')):
-  soup = bs4.BeautifulSoup(open('html/' + year).read(), 'html.parser')
-  medalists = None
-  for evt in events:
-    td = soup.select_one('td:-soup-contains("{}")'.format(evt))
-    if td:
-      evtmedals = { a.text for a in td.parent.find_all('a', href = re.compile('/wiki/.*_at_the_.*_Summer_Olympics$')) }
-      if medalists is None: medalists = evtmedals
-      else: medalists = { cnt for cnt in medalists if cnt in evtmedals }
-  if (len(medalists) > 0): print(year, medalists)
+for women in [ False, True ]:
+  print('Women' if women else 'Men')
+  for year in sorted(os.listdir('html')):
+    soup = bs4.BeautifulSoup(open('html/' + year).read(), 'html.parser')
+    if women:
+      try: soup = soup.find('span', id = 'Women').find_next('table')
+      except: continue
+    medalists = None
+    for evt in events:
+      td = soup.select_one('td:-soup-contains("{}")'.format(evt))
+      if td:
+        evtmedals = { a.text for a in td.parent.find_all('a', href = re.compile('/wiki/.*_at_the_.*_Summer_Olympics$')) }
+        if medalists is None: medalists = evtmedals
+        else: medalists = { cnt for cnt in medalists if cnt in evtmedals }
+    if (len(medalists) > 0): print(year, medalists)
