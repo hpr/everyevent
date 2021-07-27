64ed69a0cae8d4296f0809c2394b83a9c0133914
Harry P <habs@sdf.org>
2021-07-27 00:43:27 -0400

    fix mutating list

--- everyevent.py
+++ everyevent.py
@@ -9,7 +9,7 @@ events = [
   '800 metres',
   '1500 metres',
   '5000 metres',
-  '10,000 metres'
+  '10,000 metres',
 ]
 
 for year in sorted(os.listdir('html')):
@@ -20,7 +20,5 @@ for year in sorted(os.listdir('html')):
     if td:
       evtmedals = { a.text for a in td.parent.find_all('a', href = re.compile('/wiki/.*_at_the_.*_Summer_Olympics$')) }
       if medalists is None: medalists = list(evtmedals)
-      else:
-        for country in medalists:
-          if not(country in evtmedals): medalists.remove(country)
+      else: medalists = [ cnt for cnt in medalists if cnt in evtmedals ]
   if (len(medalists) > 0): print(year, medalists)
